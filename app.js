09f8c3390159587e2c85dc8e750e592c9dd3514d
aadesh-code <agajbhiye725@gmail.com>
2025-04-03 18:56:10 +0530

    intial commit

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ import ReactDOM from "react-dom/client";
 
 const heading = React.createElement("h1", {}, "Hello world from react");
 
+console.log(heading);
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(heading);
